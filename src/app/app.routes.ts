9e14b59f1fcca33860ec602cb22eeddcaf2ca857
Daniel Tinazzi <daniel.tinazzi@outlook.com>
2024-01-22 17:32:26 +0100

    Add review page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ReviewComponent } from './pages/review/review.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'review',
+    component: ReviewComponent,
+  }
+];
