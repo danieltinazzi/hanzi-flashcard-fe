5d43734f9e5e2b36370bc975b27dc13ce7a739fe
Daniel Tinazzi <daniel.tinazzi@outlook.com>
2024-01-22 18:53:56 +0100

    Add review page UI

--- src/app/pages/review/review.component.ts
+++ src/app/pages/review/review.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-review',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatCardModule,
+  ],
   templateUrl: './review.component.html',
   styleUrl: './review.component.scss'
 })
 export class ReviewComponent {
 
+  currentInput: string = '';
 }
