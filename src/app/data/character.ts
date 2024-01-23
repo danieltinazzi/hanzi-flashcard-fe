c23f8d196ebd3fe9c3e1f81474c765c77432a8e6
Daniel Tinazzi <daniel.tinazzi@outlook.com>
2024-01-23 20:13:03 +0100

    Add review stepper

---
+++ src/app/data/character.ts
@@ -0,0 +1,5 @@
+export interface Character {
+    simplified: string;
+    traditional: string;
+    pinyin: string[];
+}
--- src/app/pages/review/review.component.ts
+++ src/app/pages/review/review.component.ts
@@ -1,22 +1,58 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Validators, FormsModule, ReactiveFormsModule, FormControl } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatStepperModule } from '@angular/material/stepper';
 import { MatCardModule } from '@angular/material/card';
+import { Character } from '../../data/character';
+import { StepperSelectionEvent } from '@angular/cdk/stepper';
 
 @Component({
   selector: 'app-review',
   standalone: true,
   imports: [
     FormsModule,
-    MatInputModule,
-    MatFormFieldModule,
+    MatButtonModule,
     MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatStepperModule,
+    ReactiveFormsModule,
   ],
   templateUrl: './review.component.html',
   styleUrl: './review.component.scss'
 })
 export class ReviewComponent {
 
-  currentInput: string = '';
+  characters: Character[] = [];
+  initialized: boolean = false;
+  inputFormControl = new FormControl('', [Validators.required]);
+
+  ngOnInit() {
+    // mockup
+    this.characters = [
+      {
+        simplified: '大',
+        traditional: '大',
+        pinyin: [ 'dà' ],
+      },
+      {
+        simplified: '人',
+        traditional: '人',
+        pinyin: [ 'rén' ],
+      },
+      {
+        simplified: '有',
+        traditional: '有',
+        pinyin: [ 'yŏu' ],
+      },
+    ];
+    this.initialized = true;
+  }
+
+  onStepChange(event: StepperSelectionEvent) {
+    this.inputFormControl.setValue('');
+    this.inputFormControl.reset();
+  }
 }
