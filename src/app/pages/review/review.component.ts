ea8b727a0dc702ea54518f2f5a3286a39d076efc
Daniel Tinazzi <daniel.tinazzi@outlook.com>
2024-01-23 22:01:39 +0100

    Add step validation

--- src/app/pages/review/review.component.ts
+++ src/app/pages/review/review.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { Validators, FormsModule, ReactiveFormsModule, FormControl } from '@angular/forms';
+import { Component, ViewChild } from '@angular/core';
+import { Validators, FormsModule, ReactiveFormsModule, FormControl, ValidatorFn, AbstractControl, ValidationErrors, FormGroupDirective, NgForm } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -7,6 +7,7 @@ import { MatStepperModule } from '@angular/material/stepper';
 import { MatCardModule } from '@angular/material/card';
 import { Character } from '../../data/character';
 import { StepperSelectionEvent } from '@angular/cdk/stepper';
+import { ErrorStateMatcher } from '@angular/material/core';
 
 @Component({
   selector: 'app-review',
@@ -25,9 +26,14 @@ import { StepperSelectionEvent } from '@angular/cdk/stepper';
 })
 export class ReviewComponent {
 
+  currentStepIndex: number = 0;
   characters: Character[] = [];
   initialized: boolean = false;
-  inputFormControl = new FormControl('', [Validators.required]);
+
+  inputFormControl = new FormControl('', [this.createValidator()]);
+  errorStateMatcher = new CustomErrorStateMatcher();
+  
+  @ViewChild('inputForm', { static: true }) inputForm: NgForm | undefined;
 
   ngOnInit() {
     // mockup
@@ -54,5 +60,45 @@ export class ReviewComponent {
   onStepChange(event: StepperSelectionEvent) {
     this.inputFormControl.setValue('');
     this.inputFormControl.reset();
+  }
+
+  createValidator(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const value = control.value;
+      if (!value) {
+        return {required: true};
+      }
+
+      const character = this.characters[this.currentStepIndex];
+      if (character.pinyin.includes(value)) {
+        return null;
+      }
+
+      const suggestions = character.pinyin.filter(s => {
+        return this.removeDiacritics(s) == this.removeDiacritics(value)
+      });
+      if (suggestions.length > 0) {
+        return {
+          partial: true,
+          suggestions,
+        };
+      }
+      return {wrong: true};
+    }
+  }
+
+  removeDiacritics(s: string) {
+    return s.normalize('NFD').replace(/\p{Diacritic}/gu, '');
+  }
+
+  onSubmit(event: SubmitEvent) {
+    this.inputFormControl.markAsPristine();
+  }
+}
+
+export class CustomErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && isSubmitted && !control.dirty);
   }
 }
